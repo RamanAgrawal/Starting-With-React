b4a778f22913e0c84c3cc80e1390cfaa76b36e7c
RamanAgrawal <iramanagrawal@gmail.com>
2023-03-07 11:44:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItems from "./components/ExpenseItems";
+
+function App() {
+  
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      
+      <ExpenseItems expense></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,9 @@
+const ExpenseItems = () => {
+    return (<div>
+    <div>food 100rs</div>
+    <div>petrol 100rs</div>
+    <div>movie 100rs</div>
+    </div>
+    )
+}
+export default ExpenseItems
