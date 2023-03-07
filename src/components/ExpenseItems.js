4b670ef4f686758edf92d4d040551190811516f2
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-03-07 13:55:50 +0530

    adding variable

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,8 +1,19 @@
+
+import './ExpenseItem.css'
 const ExpenseItems = () => {
-    return (<div>
-    <div>food 100rs</div>
-    <div>petrol 100rs</div>
-    <div>movie 100rs</div>
+
+
+    const expenseDate = new Date(2023, 2, 3);
+    const expenseDescription = 'pizza hut'
+    const expensePrice = 600
+    const LocationOfExpenditure = 'Bhilai'
+
+    return (<div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2 className='expense-item h2'>{expenseDescription}</h2>
+            <h2>{LocationOfExpenditure}</h2></div>
+        <div className='expense-item__price ' >{expensePrice}</div>
     </div>
     )
 }
