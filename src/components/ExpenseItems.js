02c3e498900a369b3c907546ffdffb93f4749891
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-03-07 15:29:07 +0530

    using map

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,20 +1,26 @@
 
 import './ExpenseItem.css'
-const ExpenseItems = () => {
+const ExpenseItems = (props) => {
+
+    return (
+        <>
+            {
+                props.expenses.map((value) => (
+                    <div className="expense-item">
+                        <div>{value.date.toISOString()}</div>
+                        <div className="expense-item__description">
+                            <h2 className='expense-item h2'>{value.title}</h2>
+                        </div>
+                        <div className='expense-item__price ' >{value.amount}</div>
+                    </div>
+
+                )
+                )
 
 
-    const expenseDate = new Date(2023, 2, 3);
-    const expenseDescription = 'pizza hut'
-    const expensePrice = 600
-    const LocationOfExpenditure = 'Bhilai'
-
-    return (<div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <div className="expense-item__description">
-            <h2 className='expense-item h2'>{expenseDescription}</h2>
-            <h2>{LocationOfExpenditure}</h2></div>
-        <div className='expense-item__price ' >{expensePrice}</div>
-    </div>
+            }
+        </>
     )
+
 }
 export default ExpenseItems
