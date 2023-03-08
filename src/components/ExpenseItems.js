3d64e6a8a36b6b6e7a02a3f4997454e78db27493
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-03-08 12:59:44 +0530

    added delete button

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,7 +2,11 @@ import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 import './ExpenseItem.css'
 const ExpenseItems = (props) => {
-
+   const removeItem=(e)=>{
+             
+             const parent=e.target.parentElement.parentElement;
+             parent.removeChild(e.target.parentElement)
+    }
     return (
         <>
             {
@@ -10,6 +14,7 @@ const ExpenseItems = (props) => {
                     <div className="expense-item">
                         <ExpenseDate date={value.date}/>
                         <ExpenseDetails title={value.title} amount={value.amount}/>
+                        <button onClick={removeItem}>delete</button>
                     </div>
 
                 )
