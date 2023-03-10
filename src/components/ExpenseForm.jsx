982d6a922707c6d0d83df6f1475bb78fff811170
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-03-10 23:06:21 +0530

    added hide functionality to form

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,53 +1,67 @@
 import { useState } from 'react';
 import './ExpenseForm.css'
 
-const ExpenseForm=()=>{
+const ExpenseForm=(props)=>{
 
     const [enteredTitle,setEnteredTitle]=useState('')
     const [enteredAmount,setEnteredAmount]=useState('')
     const [enteredDate,setEnteredDate]=useState('')
     const TitleChangeHandler=(e)=>{
         setEnteredTitle(e.target.value);
-        // console.log(e.target.value);
     }
     const AmountChangeHandler=(e)=>{
         setEnteredAmount(e.target.value);
-        // console.log(e.target.value);
     }
     const DateChangeHandler=(e)=>{
         setEnteredDate(e.target.value);
-        // console.log(e.target.value);
     }
     const SubmitHandler =(e)=>{
         e.preventDefault();
         const expenseData={
+            
             title:enteredTitle,
             amount:enteredAmount,
             date:new Date(enteredDate)
         }
-        
-        console.log(expenseData);
+        props.onNewExpense(expenseData)
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+        // console.log(expenseData);
     }
-return <form action="" onSubmit={SubmitHandler}>
+    const showForm=(e)=>{
+        e.target.previousElementSibling.style.display="block"
+        e.target.style.display="none"
+        console.log(e.target.previousElementSibling);
+    }
+    const hideForm=(e)=>{
+        e.target.parentElement.parentElement.style.display="none"
+        e.target.parentElement.parentElement.nextElementSibling.style.display=""
+    }
+return <><form action="" onSubmit={SubmitHandler}>
     <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Type</label>
-            <input type="text" onChange={TitleChangeHandler}/>
+            <input type="text" value={enteredTitle} onChange={TitleChangeHandler}/>
         </div>
         <div className="new-expense__control">
             <label>amount</label>
-            <input type="number" onChange={AmountChangeHandler}/>
+            <input type="number" value={enteredAmount} onChange={AmountChangeHandler}/>
         </div>
         <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min='2020-02-16' onChange={DateChangeHandler}/>
+            <input type="date" min='2020-02-16' value={enteredDate} onChange={DateChangeHandler}/>
         </div>
     </div>
     <div className="new-expense__actions">
+        <button onClick={hideForm}>Cancel</button>
         <button type='submit'>Add Expense</button>
     </div>
+    
 
 </form>
+<button className='new-expense__actions' onClick={showForm}>Add Expense</button>
+</>
 }
 
 export default ExpenseForm
