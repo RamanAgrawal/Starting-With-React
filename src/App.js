d74af686198a56895a416257ec807029a6be867a
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-03-10 17:36:53 +0530

    added addExpenseHandler()

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import Expenses from "./components/Expenses/Expenses";
-const App=()=> {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+const demoExpenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  
+];
+const App = () => {
+  const [expenses, setExpenseHandler] = useState(demoExpenses)
+  const addExpenseHandler = (addExpenseData) => {
+    // console.log(addExpenseData);
+    setExpenseHandler((expense) => {
+      console.log([addExpenseData, ...expense]);
+      return [addExpenseData, ...expense];
+      
+    });
+    
+  };
+
   return (
-  <div>
-    <h2>Let's get started!</h2>
-    <Expenses expenses={expenses} />
-  </div>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
   )
 }
 
