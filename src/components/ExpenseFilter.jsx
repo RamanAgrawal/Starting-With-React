e6868039fd08538fee8af049ad0868bcf5304300
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-03-10 17:46:42 +0530

    added expenseFilter

---
+++ src/components/ExpenseFilter.jsx
@@ -0,0 +1,24 @@
+import './ExpenseFilter.css'
+
+
+const ExpenseFilter=(props)=>{
+const onChangeHandler=(e)=>{
+    props.onChangeFilter(e.target.value)
+}
+
+return (
+    <div className='expenses-filter'>
+      <div className="expenses-filter__control">
+        <label >Filter by Year</label>
+        <select value={props.selected} onChange={onChangeHandler} >
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+        </select>
+        </div>
+    </div>
+)
+}
+
+export default ExpenseFilter;
